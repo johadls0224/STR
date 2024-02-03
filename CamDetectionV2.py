0cd94db160635c65677bc5add5cdc8187e8bdf12
Jhoan J De Los Santos P <jojavierdelossantos@gmail.com>
2024-02-03 13:18:16 -0400

    Add files via upload
    MODIFICACION DE SEGMENTACION

--- CamDetectionV2.py
+++ CamDetectionV2.py
@@ -1,22 +1,17 @@
 import cv2
 import numpy as np
 
-# Captura de video desde la cámara del móvil
 cap = cv2.VideoCapture(0)
 i = 0
 
-# # Primer fotograma como referencia
-# ret, fondo = cap.read()
-
 while (True):
-    # Lectura de cada fotograma
     ret, frame = cap.read()
     if ret == False:break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    if i == 15:
+    if i == 20:
         bgGray = gray
-    if i > 15:
+    if i > 20:
         dif = cv2.absdiff(gray, bgGray)
         _, th = cv2.threshold(dif, 40, 255, cv2.THRESH_BINARY)
         cnts,_ = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -27,7 +22,7 @@ while (True):
             if area > 9000:
                 x,y,w,h = cv2.boundingRect(c)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                #(frame, (x,y), (x+w,y+h), (255,0,0),2)
+                #(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 print("Detectando movimiento...PIIIII")
 
     cv2.imshow('video1', frame)
